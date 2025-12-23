3d2406d137014bd2b1b8c61791085eceab50f3ae
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-23 18:10:47 +0000

    Upload: resource_1_albany_cavalcade_iii_add_on_tuning_liveries_lods/fxmanifest.lua

---
+++ resource_1_albany_cavalcade_iii_add_on_tuning_liveries_lods/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'Converted Resource: albany-cavalcade-iii-add-on-tuning-liveries-lods'
+author 'GTA5Mods to FiveM Converter'
+version '1.0.0'
+
+-- This resource was converted from GTA5Mods.com
+-- Original mod: albany-cavalcade-iii-add-on-tuning-liveries-lods
+
+client_scripts {
+    -- Add client scripts if any
+}
+
+server_scripts {
+    -- Add server scripts if any
+}
+
+files {
+    -- Add required files
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/**/*.ytyp'
